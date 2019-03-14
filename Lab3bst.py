72de95a73657939ec00cbfe5d070409b83c482d9
luisren64 <47256742+luisren64@users.noreply.github.com>
2019-03-14 16:22:50 -0600

    Add files via upload

--- Lab3bst.py
+++ Lab3bst.py
@@ -154,17 +154,19 @@ B = [10, 4, 15, 18, 12, 2, 8, 1, 3, 5, 9, 7]
 for b in B:
     T2 = Insert(T2, b)
 C = [1, 2, 3, 4, 5, 7, 8, 9, 10, 12, 15, 18] 
-#InOrder(T)
-#print()
+
+
 #InOrderD(T,'')
 #print()
-ListTree = None
-#print(IterateSearch(T,15))
+#ListTree = None
+print(IterateSearch(T,50))
+
 List = []
 TreeToList(T,List)
 print(List)
+
 PrintByDepth(T2,0)
 print()
 InOrderD(T2,' ')
-ListToTree(ListTree, C)
+#ListToTree(ListTree, C)
 #InOrderD(ListTree)
